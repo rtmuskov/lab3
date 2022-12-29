b1859cd4b6fd9945f99352e6a0d96844b1115b94
Ghost2021 <rtmuskov@gmail.com>
2022-12-29 13:13:26 +0300

    5

--- add.c
+++ add.c
@@ -1,67 +1,28 @@
-
 #include "add.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "input.h"
 #include "check.h"
-
-/*int add (int *h, int *mass, int **pArray) {
-    int id, flag = 1;
+int add (int *h, int *mass, int **pArray) {
+    int id; int el; int flag =1;
     while (flag == 1) {
+        printf("Print elelelement");
+        el = check();
         printf("/print index ");
         id = check();
-        if (id <= *h) {
-            *h++;
-            *pArray = realloc(mass, (*h) * sizeof(int));
-            mass = *pArray;
-            id = check();
-            printf("enter elements\n");
-            for (int i = *h - 1; i >= id - 1; i--) {
-                mass[i + 1] = mass[i];
-            }
-            mass[id - 1] = check();
-            for (int i = 0; i < *h; i++) {
-                printf("%d ", mass[i]);
-            }
-            printf("\n");
-        }
-
-
-    if (id >= *h - 1) {
-        *h++;
+        *h = *h + 1;
         *pArray = realloc(mass, (*h) * sizeof(int));
-        id = *h;
-        printf("enter elements\n");
-        mass[id - 1] = check();
-        for (int i = 0; i < *h; i++) {
-            printf("%d ", mass[i]);
+
+        if (id >= *h) id = *h - 1;
+        if (id < 0) id = 0;
+        for (int i = *h - 1; i > id; i--) {
+            (*pArray)[i] = (*pArray)[i - 1];
         }
-        printf("\n");
-
+        (*pArray)[id] = el;
+        mass = *pArray;
+        printf("\nEnter [1] to insert anything else or [0] to finish: ");
+        input(&flag);
     }
-}
-
-return 0;
-}
-*/
-int add (int *h, int *mass, int **pArray) {
-    int id;
-    printf("Print elelelement");
-    int el;
-    scanf("%d", &el);
-    printf("/print index ");
-    //id = check();
-    scanf("%d", &id);
-    *h= *h+1;
-    *pArray = realloc(mass, (*h) * sizeof(int));
-
-    if (id >= *h) id = *h -1;
-    if (id < 0) id = 0;
-    for (int i = *h - 1; i > id; i--) {
-        (*pArray)[i] = (*pArray)[i - 1];
-    }
-    (*pArray)[id] = el;
-    mass = *pArray;
     return 0;
 }
 
