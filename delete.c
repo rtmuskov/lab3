d7b8bbe1d1fecf54b61ac204759a840c8037cda5
Ghost2021 <rtmuskov@gmail.com>
2022-12-29 13:16:37 +0300

    6

--- delete.c
+++ delete.c
@@ -9,33 +9,32 @@
 #include "check.h"
 
 int delete(int *h, int *mass, int **pArray) {
-    int id, flag = 1, size = *h, sizeUsed = *h;
+    int id, flag = 1, n = *h;
     while (flag == 1) {
-        printf("\n-----------------------");
-        printf("\ndelete element at index: ");
+        printf("\nprint index: ");
         id= check();
-        if (id > sizeUsed - 1 || id < 0) {
+        if (id > n- 1 || id < 0) {
             printf("%d is out of range", id);
             continue;
         }
-        for (int i = id; i < sizeUsed - 1; i++) {
+        for (int i = id; i < n - 1; i++) {
             mass[i] = mass[i + 1];
         }
-        sizeUsed--;
-        if (sizeUsed < size -1) {
-            *pArray = realloc(mass, (size / 2) * sizeof(int));
+        n--;
+        if (n< *h -1) {
+            *pArray = realloc(mass, (*h / 2) * sizeof(int));
             if (*pArray == NULL) {
                 printf("Failed to allocate memory\n");
                 return -1;
             }
             mass = *pArray;
-            size /= 2;
+            *h/= 2;
         }
         printf("\nEnter [1] to delete anything else or [0] to finish: ");
         flag = check();
     }
 
-    *h = size;
-    *h= sizeUsed;
+
+    *h= n;
     return 0;
 }
