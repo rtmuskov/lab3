e3055d025dab26ba58d555c6a876ec16c0ae4281
Ghost2021 <rtmuskov@gmail.com>
2023-01-08 11:19:59 +0300

    7676

--- modifiedarray.c
+++ modifiedarray.c
@@ -4,7 +4,9 @@
 
 int modifiedarray(int *h, int *mass, int **pArray,int *mass2,int *h2) {
     int flag = 0;
-
+    for (int i = 0; i < *h; i++) {
+        mass2[i] = mass[i];
+    }
     int l = *h;
     for (int a = 0; a < l; a++) {
         if((mass[a] % 9 ==0 ) && (mass[a] != 0)) {
@@ -12,15 +14,14 @@ int modifiedarray(int *h, int *mass, int **pArray,int *mass2,int *h2) {
         }
         if (flag == 0) {
             for (int i = a; i < l - 1; i++) {
-                mass[i] = mass[i+1];
+                mass2[i] = mass2[i+1];
             }
             l--;
             a--;
-            *pArray= realloc(mass,l *sizeof(int));
+            *pArray= realloc(mass2,l *sizeof(int));
         }
         flag = 0;
     }
-    *h=l;
     *h2 = l;
     printf("\nНовый массив создан\n");
     return 0;
